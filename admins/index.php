8ad3e4ada0159731928c1dfba4321dd242f83efc
Mennajy <mennajy@live.fr>
2017-08-03 12:08:24 +0200

    Ajout Pages #2

--- admins/index.php
+++ admins/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<?php  include('navbar.php') ?>
-<?php  include('header.php') ?>
+<?php  include('pages/navbar.php') ?>
+<?php  include('pages/header.php') ?>
 
 <html>
   <head>
@@ -11,6 +11,211 @@
   <body>
 
   <!-- Zone De Charte -->
-  
+  <div class="row">
+      <div class="col-lg-12">
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                  <i class="fa fa-bar-chart-o fa-fw"></i> Charte d'utilisation
+                  <div class="pull-right">
+                      <div class="btn-group">
+                        <p> Test </p>
+                          </div>
+                            <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                              Actions
+                              <span class="caret"></span>
+                          </button>
+                            </div>
+                      </div>
+                    <div class="">
+                      <!-- <center> -->
+                        <b> <h1>Test </h1> </b> <br>
+                        En utilisant le site associathèque, l’utilisateur accepte de respecter les conditions d'utilisation et de navigation définies dans la présente charte. La charte pouvant être modifiée à tout moment par l'éditeur sans notification préalable, il appartient à l’utilisateur du site de la consulter régulièrement. L’utilisateur est réputé avoir accepté la dernière version à chaque nouvelle connexion au site. Toute connexion au site est subordonnée au respect de la présente charte.<br>
+
+Engagement de l’utilisateur<br>
+
+L’utilisateur s’engage à n'enfreindre aucune loi ou règlement. Sont notamment interdits, sous peine, le cas échéant, de sanctions pénales :<br>
+
+    l'atteinte à la vie privée d'autrui (citation nominative de tierces personnes sans leur accord explicite). <br>Dans ce cadre, l'envoi de messages électroniques non sollicités aux utilisateurs du site est interdit ;
+    la diffamation et l'injure ;
+    l'incitation aux crimes et délits et la provocation au suicide, la provocation à la discrimination, à la haine notamment raciale, ou à la violence ;<br>
+    l'apologie de tous les crimes, notamment meurtre, viol, crime de guerre et crime contre l'humanité ; la négation de crimes contre l'humanité ;<br>
+    la reproduction, représentation ou diffusion d'une œuvre soumise à des droits de propriété intellectuelle ne le permettant pas ;<br>
+    la publicité ou les messages ou contenus de quelque nature que ce soit à vocation commerciale ;<br>
+    les discussions traitant de la copie de logiciels commerciaux pour un usage autre qu'une copie de sauvegarde dans les conditions prévues par le code de la propriété intellectuelle.<br>
+
+                      </center>
+
+                    </div>
+                  </div>
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                  <div id="morris-area-chart"></div>
+              </div>
+              <!-- /.panel-body -->
+          </div>
+          <!-- /.panel -->
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                  <i class="fa fa-bar-chart-o fa-fw"></i> Bar Chart Example
+                  <div class="pull-right">
+                      <div class="btn-group">
+                          <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                              Actions
+                              <span class="caret"></span>
+                          </button>
+                          <ul class="dropdown-menu pull-right" role="menu">
+                              <li><a href="#">Action</a>
+                              </li>
+                              <li><a href="#">Another action</a>
+                              </li>
+                              <li><a href="#">Something else here</a>
+                              </li>
+                              <li class="divider"></li>
+                              <li><a href="#">Separated link</a>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                  <div class="row">
+                      <div class="col-lg-4">
+                          <div class="table-responsive">
+                              <table class="table table-bordered table-hover table-striped">
+                                  <thead>
+                                      <tr>
+                                          <th>#</th>
+                                          <th>Date</th>
+                                          <th>Time</th>
+                                          <th>Amount</th>
+                                      </tr>
+                                  </thead>
+
+                          <!-- /.table-responsive -->
+                      </div>
+                      <!-- /.col-lg-4 (nested) -->
+                      <div class="col-lg-8">
+                          <div id="morris-bar-chart"></div>
+                      </div>
+                      <!-- /.col-lg-8 (nested) -->
+                  </div>
+                  <!-- /.row -->
+              </div>
+              <!-- /.panel-body -->
+          </div>
+          <!-- /.panel -->
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                  <i class="fa fa-clock-o fa-fw"></i> Responsive Timeline
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                  <ul class="timeline">
+                      <li>
+                          <div class="timeline-badge"><i class="fa fa-check"></i>
+                          </div>
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                                  <p><small class="text-muted"><i class="fa fa-clock-o"></i> 11 hours ago via Twitter</small>
+                                  </p>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero laboriosam dolor perspiciatis omnis exercitationem. Beatae, officia pariatur? Est cum veniam excepturi. Maiores praesentium, porro voluptas suscipit facere rem dicta, debitis.</p>
+                              </div>
+                          </div>
+                      </li>
+                      <li class="timeline-inverted">
+                          <div class="timeline-badge warning"><i class="fa fa-credit-card"></i>
+                          </div>
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolorem quibusdam, tenetur commodi provident cumque magni voluptatem libero, quis rerum. Fugiat esse debitis optio, tempore. Animi officiis alias, officia repellendus.</p>
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maiores odit qui est tempora eos, nostrum provident explicabo dignissimos debitis vel! Adipisci eius voluptates, ad aut recusandae minus eaque facere.</p>
+                              </div>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="timeline-badge danger"><i class="fa fa-bomb"></i>
+                          </div>
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus numquam facilis enim eaque, tenetur nam id qui vel velit similique nihil iure molestias aliquam, voluptatem totam quaerat, magni commodi quisquam.</p>
+                              </div>
+                          </div>
+                      </li>
+                      <li class="timeline-inverted">
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates est quaerat asperiores sapiente, eligendi, nihil. Itaque quos, alias sapiente rerum quas odit! Aperiam officiis quidem delectus libero, omnis ut debitis!</p>
+                              </div>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="timeline-badge info"><i class="fa fa-save"></i>
+                          </div>
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis minus modi quam ipsum alias at est molestiae excepturi delectus nesciunt, quibusdam debitis amet, beatae consequuntur impedit nulla qui! Laborum, atque.</p>
+                                  <hr>
+                                  <div class="btn-group">
+                                      <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
+                                          <i class="fa fa-gear"></i> <span class="caret"></span>
+                                      </button>
+                                      <ul class="dropdown-menu" role="menu">
+                                          <li><a href="#">Action</a>
+                                          </li>
+                                          <li><a href="#">Another action</a>
+                                          </li>
+                                          <li><a href="#">Something else here</a>
+                                          </li>
+                                          <li class="divider"></li>
+                                          <li><a href="#">Separated link</a>
+                                          </li>
+                                      </ul>
+                                  </div>
+                              </div>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fuga odio quibusdam. Iure expedita, incidunt unde quis nam! Quod, quisquam. Officia quam qui adipisci quas consequuntur nostrum sequi. Consequuntur, commodi.</p>
+                              </div>
+                          </div>
+                      </li>
+                      <li class="timeline-inverted">
+                          <div class="timeline-badge success"><i class="fa fa-graduation-cap"></i>
+                          </div>
+                          <div class="timeline-panel">
+                              <div class="timeline-heading">
+                                  <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                              </div>
+                              <div class="timeline-body">
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt obcaecati, quaerat tempore officia voluptas debitis consectetur culpa amet, accusamus dolorum fugiat, animi dicta aperiam, enim incidunt quisquam maxime neque eaque.</p>
+                              </div>
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+              <!-- /.panel-body -->
+          </div>
   </body>
 </html>
