67863a213d92519620b37f6abadd4a2295906ac7
eviaud <evan.viaud@gmail.com>
2017-08-31 13:21:44 +0200

    Ajout

---
+++ cv/includes/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+//connect to mysql database
+$server = "127.0.0.1";
+$username = "vagrant";
+$password = "123";
+$db = "vagrant";
+// Création de la connexion:
+$connection = mysqli_connect( $server, $username, $password, $db );
+
+// Test de la connexion:
+if(!$connection){
+  echo "Erreur :" . mysqli_connect_error();
+  die('Impossible de se connecter à la base de données');
+}
+?>
