945a4d94f2fcca10c1d3952743e0f7baec78f7b6
eviaud <evan.viaud@gmail.com>
2017-09-15 12:10:16 +0200

    Alpha 0.0.2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,15 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CV'THEQUE</title>
+
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.css">
+
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
+
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+
+    <link rel="stylesheet" href="{{ URL::asset('./css/style.css') }}"/>
+
+
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.js"></script>
 
 </head>
-<body style="background-color: #f8f8f8;">
+<body>
 
 
 <div class="ui menu">
@@ -49,17 +61,12 @@
     </div>
 </div>
 
-<main>
-    <div class="container">
-        @if(Session::has('flash_message'))
-            <div class="alert alert-success">
-                {{ Session::get('flash_message') }}
-            </div>
-@endif
-
 
 @yield('content')
 
+@yield('footer')
 
-</body>
-</html>
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,32 @@
 @extends('layouts.master')
+
+
+
+@section('content')
+
+    <div class="head-bg">
+        <div class="head-bg-img"></div>
+        <div class="head-bg-content">
+            <div
+                class="col-lg-offset-4 col-lg-4 col-md-offset-4 col-md-4 col-sm-offset-4 col-sm-4 col-xs-offset-4 col-xs-6">
+                <div class="ui category search">
+                    <div class="ui icon input">
+                        <input class="prompt" type="text" placeholder="Recherche de CV...">
+                        <i class="search icon"></i>
+                    </div>
+                    <div class="results"></div>
+                </div>
+                <br>
+                <div>
+                    <button type="submit" class="btn btn-primary">Qui veut de la limonade?</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+@include('layouts.footer')
+
+@endsection
+
+
